36ba56963d1017f949353bf4228396ee018506cc
THIVEND <baptiste.thivend@protonmail.com>
2022-11-10 14:44:59 +0100

    Add FormName credential

--- metabase_forms_summary_automation/main.py
+++ metabase_forms_summary_automation/main.py
@@ -5,9 +5,10 @@ from metabase_forms_summary_automation import credentials
 def make_summary():
     demo_form_id = credentials.FORM_ID
     answers_model_id = credentials.ANSWERS_MODEL_ID
+    form_name = credentials.FORM_NAME
     f = FormsSummary(form_id=demo_form_id, credentials=credentials)
     f.get_questions_parameters()
 
     chart_list = f.create_question_summary()
-    dashboard = create_dashboard(f.mtb, "Open data questionnaire", f.collection_id)
+    dashboard = create_dashboard(f.mtb, form_name, f.collection_id)
     add_cards_to_dashboard(f.mtb, dashboard, chart_list)
